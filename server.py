cc051838470c0078396c2b1a9ab6cca73d5a57a7
ArthurDeSchutter <deschutterarthur1@gmail.com>
2020-09-23 10:49:41 +0200

    button setup

--- server.py
+++ server.py
@@ -1,9 +1,26 @@
 from time import sleep
 from picamera import PiCamera
+from datetime import datetime
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
-camera = PiCamera()
-camera.resolution = (1024, 768)
-camera.start_preview()
-# Camera warm-up time
-sleep(2)
-camera.capture('foo.jpg')
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+
+GPIO.setup(12,GPIO.OUT)# led GPIO
+
+
+def button_callback(channel):
+    GPIO.output(18,GPIO.HIGH)
+    print("Button was pushed!")
+
+GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+
+
+# datetime object containing current date and time
+now = datetime.now()
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print("date and time =", dt_string)
+
+

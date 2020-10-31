01e3da6ac320207af824b4aafa3190e84bf646aa
ArthurDeSchutter <deschutterarthur1@gmail.com>
2020-10-31 17:11:49 +0100

    added jsonevent database

--- server.py
+++ server.py
@@ -4,6 +4,12 @@ from time import sleep
 import picamera
 from datetime import datetime
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import json
+import pymongo
+
+#mongoDB stuff
+client = pymongo.MongoClient("mongodb+srv://APuser:Bulletbomb1@doorbellevents.xbdxu.mongodb.net/Events?retryWrites=true&w=majority")
+db = client.test
 
 #camera settings
 camera = picamera.PiCamera()
@@ -26,10 +32,25 @@ while True:
     if GPIO.input(PIR_PIN):
         print("Motion Detected!")
     time.sleep(1)
+
+
 def button_callback(channel):
-    
+    JSONEvent = {
+        "date": getTime(),
+        "filename": getTime(),
+        "photos":
+        [
+            "1" + getTime(),
+            "2" + getTime(),
+            "3" + getTime(),
+        ]
+        "favorite": False
+    }
     print("Button was pushed!")
-    record(10,getTime())
+    record(10,JSONEvent.filename)
+    #convert the dictionary in a proper JSON object
+    JSONEvent = json.dumps(JSONEvent)
+    db.Events.insert(JSONEvent)
 
 
 GPIO.add_event_detect(10,GPIO.FALLING,callback=button_callback) # Setup event on pin 10 rising edge
@@ -40,9 +61,17 @@ def record(time,filename):
     folder_Location = "/home/pi/Desktop/Windows-Share/"
     filename = folder_Location + filename + ".h264"
     print(filename)
+    #capture image and start recording
+    camera.capture("1" + filename, use_video_port=True)
     camera.start_recording(filename)
-    camera.wait_recording(int(time))
+    #wait time / 2 to take a pic and start recording again
+    camera.wait_recording(int(time/2))
+    camera.capture("1" + filename, use_video_port=True)
+    camera.wait_recording(int(time/2))
+    #stop recording and capture final image
     camera.stop_recording()
+    camera.capture("1" + filename, use_video_port=True)
+
     
     GPIO.output(12,GPIO.LOW)
     
